a37e9a9002b1e80dd02c27f1dc62152383b28030
saiful10400 <saifulislam10400i5@gmail.com>
2023-12-20 17:13:13 +0600

    new author adding api done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const cookieParser=require("cookie-parser")
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors=require("cors")
 require('dotenv').config()
 const port=process.env.port || 5000
 
@@ -8,7 +9,7 @@ const port=process.env.port || 5000
 const app =express()
 // all middlewares.
 app.use(express.json())
-
+app.use(cors())
 
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.qe6izo7.mongodb.net/?retryWrites=true&w=majority`;
@@ -26,14 +27,27 @@ async function run() {
   try {
 
 
-
+const database=client.db("Public_library")
     // all api is form here.
 app.get("/", async(req,res)=>{
     res.send(`this server is running on ${port} port`)
 })
 
+// get authors.
+const authorCollections=database.collection("Authors")
+app.get("/authors" ,async(req,res)=>{
+  const result=await authorCollections.find().toArray()
+  res.send(result)
+})
+// post new author
+app.post("/post_author",async(req,res)=>{
+  const data=req.body
+  const result=await authorCollections.insertOne(data)
+  res.send(result)
+})
 
 
+//                   end           .
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
